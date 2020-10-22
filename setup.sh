352f5d8943dae124e973dac3269dad3b0a770ce5
anupamreddy <48361353+anupamreddy@users.noreply.github.com>
2020-10-22 13:44:08 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email=\"anupamreddybadveli@gmail.com\"\n\
-" > ~/.streamlit/credentials.tom1
-
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-
-"> ~/.streamlit/config.tom1
+" > ~/.streamlit/config.toml
